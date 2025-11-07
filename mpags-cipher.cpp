ca346d76ef45d53d3cf4599684506647b900807f
J-I-Martin <jordan02martin@gmail.com>
2025-11-07 14:43:02 +0000

    completed week 4

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,9 +1,60 @@
+#include <algorithm>    // for std::find method
 #include <cctype>
 #include <iostream>
 #include <string>
+#include <vector>
 
-int main()
+int main(int argc, char* argv[])
 {
+    // Handle arguments
+    const std::vector<std::string> cmdLineArgs{argv, argv + argc};
+    const std::vector<std::string> listofArgs{"-h", "--help", "--version", "-i",
+                                              "-o"};
+
+    // -h and --help flag handling
+    const char* helptext = "Usage: mpags-cipher [-h/--help]\n"
+                           "Options:\n"
+                           "    --help: Displays this message.";
+    // change to for loop with boolean flag
+    for (std::size_t i = 1; i != cmdLineArgs.size(); i++) {
+        std::string option{cmdLineArgs[i]};
+
+        if (option == "-h" || option == "--help") {
+            std::cout << helptext << std::endl;
+            return 0;
+        }
+        if (option == "--version") {
+            std::cout << "mpags-cipher version 0.1.0" << "\n";
+            return 0;
+        }
+        if (option == "-i") {
+            const std::string input_file{cmdLineArgs[i + 1]};
+            i++;
+            if (std::find(listofArgs.begin(), listofArgs.end(), input_file) !=
+                listofArgs.end()) {
+                // if the element following -i is another option
+                std::cout << "Argument Error: missing input file" << "\n";
+                return 1;
+            } else if (input_file == "") {
+                std::cout << "Argument Error: missing input file" << "\n";
+                return 1;
+            }
+        }
+        if (option == "-o") {
+            const std::string output_file{cmdLineArgs[i + 1]};
+            i++;
+            if (std::find(listofArgs.begin(), listofArgs.end(), output_file) !=
+                listofArgs.end()) {
+                // if the element following -i is another option
+                std::cout << "Argument Error: missing output file" << "\n";
+                return 1;
+            } else if (output_file == "") {
+                std::cout << "Argument Error: missing output file" << "\n";
+                return 1;
+            }
+        }
+    }
+
     // Initialise variables
     char c{'x'};
     std::string out_text;
